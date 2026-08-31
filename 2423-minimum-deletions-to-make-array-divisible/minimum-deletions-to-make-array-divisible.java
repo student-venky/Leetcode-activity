6ef28c9f1356da1fac65c846c12b32ca5a8b3c29
Venkata Sai Charan <venkatasaicharan72@gmail.com>
2026-08-31 21:16:01 +0530

    Time: 272 ms (5.06%) | Memory: 117.4 MB (5.06%) - LeetSync

--- 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.java
+++ 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.java
@@ -2,7 +2,7 @@ class Solution {
     public int minOperations(int[] nums, int[] numsDivide) {
         HashMap<Integer,Integer> map=new HashMap<>();
         HashSet<Integer> set=new HashSet<>();
-        List<Integer> l=new ArrayList<>();
+        //List<Integer> l=new ArrayList<>();
         PriorityQueue<Integer> pq=new PriorityQueue<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
@@ -13,15 +13,15 @@ class Solution {
         for(int x:map.keySet()){
             pq.add(x);
         }
-        for(int x:set){
-            l.add(x);
-        }
+        // for(int x:set){
+        //     l.add(x);
+        // }
         int numberofdeletions=0;
         while(!pq.isEmpty()){
             int ele=pq.poll();
             boolean b=true;
             if(b){
-                for(int i:l){
+                for(int i:numsDivide){
                     if(i%ele!=0){
                         b=false;
                         numberofdeletions+=map.get(ele);
